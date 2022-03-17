9f080826a4a0cd50487a6abedd62e071ba40b1e8
ocean-tech-developer <ocean.techsea@gmail.com>
2022-03-17 07:03:00 -0700

    fix qr code

---
+++ build/static/js/5.91333d9f.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[5],{993:function(p,s){}}]);
+//# sourceMappingURL=5.91333d9f.chunk.js.map
---
+++ build/static/js/6.8b36eeb6.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[6],{1114:function(p,s){}}]);
+//# sourceMappingURL=6.8b36eeb6.chunk.js.map
