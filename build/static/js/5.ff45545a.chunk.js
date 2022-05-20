b6c88be13845d1c152d6d88cde27ce8caf1ebde8
ocean-tech-developer <ocean.techsea@gmail.com>
2022-05-20 06:37:06 +0200

    deploy

---
+++ build/static/js/5.ff45545a.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpchadinu=this.webpackJsonpchadinu||[]).push([[5],{1015:function(n,c){}}]);
+//# sourceMappingURL=5.ff45545a.chunk.js.map
---
+++ build/static/js/6.30afdf08.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpchadinu=this.webpackJsonpchadinu||[]).push([[6],{1134:function(n,c){}}]);
+//# sourceMappingURL=6.30afdf08.chunk.js.map
